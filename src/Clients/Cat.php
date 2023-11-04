ec335b7decb770760d198df444650ad3ea304367
renmichaela <jakesuellentrop@gmail.com>
2023-11-04 06:47:25 +0000

    adds Cat Client methods

--- src/Clients/Cat.php
+++ src/Clients/Cat.php
@@ -4,8 +4,48 @@ namespace Vibrant\Clients;
 
 class Cat extends Client
 {
+    const BASE_URL = 'https://cataas.com/cat';
+
     public function __construct()
     {
-        $this->url = 'https://cataas.com/cat';
+        $this->url = self::BASE_URL;
+    }
+
+    /**
+     * Return a random cat gif
+     * 
+     * @return Cat
+     */
+    public function gif()
+    {
+        $this->url = self::BASE_URL."/gif";
+
+        return $this;
+    }
+
+    /**
+     * Return a random cat captioned with the provided text
+     * 
+     * @param text
+     * @return Cat
+     */
+    public function says(string $text)
+    {
+        $this->url = self::BASE_URL."/says/".$text;
+
+        return $this;
+    }
+
+    /**
+     * Return a random cat with the provided tag
+     * 
+     * @param tag
+     * @return Cat
+     */
+    public function tag(string $tag)
+    {
+        $this->url = self::BASE_URL."/".$tag;
+
+        return $this;
     }
 }
